0b5157adcd104a5bf3dc069f31b4891cd5f328ae
Shavkat~ <shavkatbekq69@gmail.com>
2023-05-07 15:52:00 +0300

    set up redux thunk and json server

--- src/redux/sneakersReducer.js
+++ src/redux/sneakersReducer.js
@@ -1,38 +1,47 @@
 const initialState = {
-    isLoading: false,
-    favorites: [],
-    orders:[],
+  isLoading: false,
+  allSneakers: [],
+  favorites: [],
+  orders: [],
+};
+
+const addTofavourite = (state, item) => {
+  return {
+    ...state,
+    favorites: [...state.favorites, item],
   };
-  
-  const addTofavourite = (state, item) => {
-    console.log(item);
-    return {
-      ...state,
-      favorites: [...state.favorites, item],
-    };
-  };
-  function addToOrders(state, item) {
-    console.log(item);
-    return { ...state, orders: [...state.orders, item] };
+};
+
+function addToOrders(state, item) {
+
+  return { ...state, orders: [...state.orders, item] };
+}
+function removeFromOrders(state, item) {
+
+  const newOrders = state.orders.filter((element) => item !== element);
+  return { ...state, orders: newOrders };
+}
+
+const saveAllSneakers = (state, allSneakers = []) => {
+  return {
+    ...state,
+    allSneakers: allSneakers,
   }
-  function removeFromOrders(state, item) {
-    console.log(item);
-    const newOrders = state.orders.filter((element) => item !== element);
-    return { ...state, orders: newOrders };
-  }
- 
-  
-  const sneakersReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'ADD_TO_FAVOURITE':
-        return addTofavourite(state, action.payload);
-        case "ADD_TO_ORDERS":
+}
+
+const sneakersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVOURITE':
+      return addTofavourite(state, action.payload);
+    case "ADD_TO_ORDERS":
       return addToOrders(state, action.payload);
-        case "REMOVE_FROM_ORDERS":
+    case "REMOVE_FROM_ORDERS":
       return removeFromOrders(state, action.payload);
-      default:
-        return state;
-    }
-  };
-  
-  export default sneakersReducer;
+    case 'SAVE_ALL_SNEAKERS':
+      return saveAllSneakers(state, action.payload);
+    default:
+      return state;
+  }
+};
+
+export default sneakersReducer;
---
+++ src/redux/thunk.js
@@ -0,0 +1,13 @@
+
+
+
+export const fetchItems = (url, ACTION_NAME) => async (dispatch) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        dispatch({ type: ACTION_NAME, payload: data });
+    } catch (error) {
+        console.error('ERRORNFETCH-ITEM');
+    }
+
+}
